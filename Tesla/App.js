fdd31cee4128ca837fd898e6250f8aea19d88cac
Abhinav Namdeo <abhaynam22@gmail.com>
2021-12-21 15:43:19 +0530

    Created custom component

--- Tesla/App.js
+++ Tesla/App.js
@@ -1,19 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem/index'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.carContainer}>
-        <ImageBackground
-          source={require('./assets/Images/ModelS.jpeg')}
-          style={styles.image}
-        />
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subTitle}>Starting at $69,480</Text>
-        </View>
-      </View>
+      <CarItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,27 +18,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  carContainer: {
-    width: '100%',
-    height: '100%'
-  },
-  titles: {
-    marginTop: '30%',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: '600'
-  },
-  subTitle: {
-    fontSize: 16,
-    color: '#5c5e62'
-  },
-  image:{
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    resizeMode: 'cover'
-  }
+  
 
 });
